8777ff75667a9e46033331ca335daa3479f25a8b
Mac.ZY <zhenying@MacZYs-MacBook-Pro.local>
2021-03-03 15:26:02 -0500

    Added firebase.js
    This file wouldn't be modified at any time. However, the authentication can be change, So you can test it with your firebase account.

---
+++ comfybake-app/src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+//This is my firebase authentication
+//you can change it to your firebase authentication
+var firebaseApp = {
+    apiKey: "AIzaSyCUU8fZTwRDsheZKGUwNDKlbMChILuW6Uc",
+    authDomain: "comfybake-d5d50.firebaseapp.com",
+    projectId: "comfybake-d5d50",
+    storageBucket: "comfybake-d5d50.appspot.com",
+    messagingSenderId: "800786672334",
+    appId: "1:800786672334:web:4924a9ea440cc72bae7e9f"
+};
+
+const database = firebase.initializeApp(firebaseApp);
+
+export default database;
