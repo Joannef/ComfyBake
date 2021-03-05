af27b3ece5aa22b1f5c81fbb69bde76d34a31ee2
Mac.ZY <zhenying@MacZYs-MacBook-Pro.local>
2021-03-05 13:54:24 -0500

    add firebase.files

--- comfybake-app/src/Routes.js
+++ comfybake-app/src/Routes.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 
 export default function Routes() {
   return (
@@ -9,9 +11,20 @@ export default function Routes() {
       <Route exact path="/">
         <Home />
     </Route>
+      
+      <Route exact path="/login">
+        <Login />
+      </Route>
+
+      <Route exact path="/signup">
+        <Signup />
+      </Route>
+
       <Route>
         <NotFound />
     </Route>
+
+    
     </Switch>
   );
 }
