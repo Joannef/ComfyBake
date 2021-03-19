6e89a49ec60e6490038094c2483fe0119df10ead
cisco-3 <francisco.ruiz34@myhunter.cuny.edu>
2021-03-19 11:38:43 -0400

    stuck on firebase-order_page connection

--- comfybake-app/src/containers/OrderCon.js
+++ comfybake-app/src/containers/OrderCon.js
@@ -4,15 +4,24 @@ import BorderWrapper from 'react-border-wrapper'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./OrderCon.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import ViewCart from "./ViewCart";
+import "./ViewCart";
 
-const db = database.firestore();
-const userCollection = db.collection("users");
+//const db = database.firestore();
+//const userCollection = db.collection("users");
+//const [orderArray, setOrderArray] = useState(false);
+const fakeURL1 = "";
+const fakeURL2 = "";
+const fakeURL3 = "";
 
 export default function OrderCon() {
-        return(
-            <div className="OrderCon text-center">
+    var orderArray = [fakeURL1, fakeURL2, fakeURL3]; //somehow creates an array of the picture url data for a users cart
+    return(
+        <section>
+            <h1 classname="checkout">Ready To Checkout?</h1>
+            <div className="right_button">
                 <Link to="/orders/cart">View Cart</Link>
             </div>
-        )
+            
+        </section>
+    )
 };
