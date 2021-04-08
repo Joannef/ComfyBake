ab3b828d025e6ad8c45807fbf6d956121556a443
cisco-3 <francisco.ruiz34@myhunter.cuny.edu>
2021-04-08 17:04:13 -0400

    some order page progress

--- comfybake-app/src/containers/OrderCon.js
+++ comfybake-app/src/containers/OrderCon.js
@@ -1,4 +1,4 @@
-import { getDefaultNormalizer } from "@testing-library/dom";
+import { getDefaultNormalizer, queryByTestId } from "@testing-library/dom";
 import React, {useState, useEffect} from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import database from '../firebase';
@@ -6,39 +6,40 @@ import "./OrderCon.css";
 
 const Email = "joannefung120@gmail.com"; //should be authenticated by logged-in user dummy user fir now
 
-function OrderBlock(order) {
-    const img = order.image;
-    const description = order.description;
-    const qty = order.qty;
-    const avail = order.avail;
-    const price = order.price;
-    const date = order.date;
-    const id = order.date;
-    const addy = order.date;
-    const pay_plan = order.date;
-    const complete = order.datel;
-
+function OrderBlock(xqty,ximg,xdes,xavail,xprice,xdate,xID,xaddy) {
+    let qty = xqty
+    let avail = xavail
+    let price = xprice
+    let date = xdate
+    let ID = xID
+    let addy = xaddy
+    let img = ximg
+    let des = xdes;
+    console.log(addy);
     return (
     <div>
       <p class="purchase-date"> Purchased on {date} </p>
-      <div class="rectangle"/>
+      {/*<div class="rectangle"/>*/}
       <div class="qty"> {qty}</div>
-      <div class="status"> {complete}</div>
+      <div class="status"> Complete</div>
+      {/* I have to make a function to display the images correctly
       <div class="img">{img}</div>
-      <div class="description">{description}</div>
+      */}
+      {/* I have to make a function to display the images correctly
+      <div class="description">{description}</div>*/}
       <div class="qty">{qty}</div>
       <div class="avail">{avail}</div>
       <div class="price">{price}</div>
       <div class="order-details">
-                <p class="ID"> {id} </p>
+                <p class="ID"> {ID} </p>
                 <p class="total"> Total</p>
                 <p class="price">{price}</p>
                 <div class="detail-btn">
                     <Link to={{ pathname: "/orders/order-details", state: {
-                                                                        id: id,
+                                                                        id: ID,
                                                                         addy: addy,
-                                                                        complete: complete,
-                                                                        plan: pay_plan,
+                                                                        complete: "True",
+                                                                        plan: "how do you intend to pay",
                                                                         price: price}
                             }}> 
                             See Order Details
@@ -48,15 +49,32 @@ function OrderBlock(order) {
     </div>
     );
   }
-//
+
 export default function OrderCon() {
     const [orderList, setOrderList] = useState([]);
+    const [orderQty, setOrderQty] = useState([]);
+    const [orderImg, setImgList] = useState([]);
+    const [orderD, setDList] = useState([]);
+    const [orderAvail, setAvailList] = useState([]);
+    const [orderPrice, setOrderPrice] = useState([]);
+    const [orderDate, setOrderDate] = useState([]);
+    const [orderID, setOrderID] = useState([]);
+    const [orderAddy, setOrderAddy] = useState([]);
     const orders = database.firestore().collection(`/users/${Email}/orders`).where('complete', '==', true).get().then(
-    (querySnapshot => (
-        setOrderList(querySnapshot.docs)
+    (querySnapshot => {
+        setOrderQty(querySnapshot.docs.map(doc=> doc.get("qty")))
+        setAvailList(querySnapshot.docs.map(doc=> doc.get("avail")))
+        setOrderPrice(querySnapshot.docs.map(doc=> doc.get("qty")))
+        setOrderID(querySnapshot.docs.map(doc=> doc.id))
+        setOrderDate(querySnapshot.docs.map(doc=> doc.get("date")))
+        //setImgList(querySnapshot.docs.map(doc=>doc.get("img"))).map(doc=>orderImg.concat(doc))
+        //setDList(querySnapshot.docs.map(doc=>doc.get("description"))).map(doc=>orderD.concat(doc))
+        setOrderAddy(querySnapshot.docs.map(doc=> doc.get("addy")))
+        setOrderList(querySnapshot.docs.map(doc=> doc.data()))
+        //you have to set all the variables in side this loop bc it wont work otherwise
         //do css for the page
         //FINITIO
-    )));
+    }));
     return(
         <section>
             <div class="right_button">
@@ -64,10 +82,13 @@ export default function OrderCon() {
             </div>
             <p class="top-left">Ready To Checkout?</p>
             <div>
-                {orderList.length > 0 ? (
-                    <div class="order-tower">
-                        {orderList.map((order) => (
-                            <OrderBlock order={order.data()}/>
+                {orderList.length != 0 ? (
+                    <div class="order-tower"> qelp
+                        {orderList.forEach((o) => (
+                            <OrderBlock o={{xqty:`${orderQty}`, ximg:`${orderImg}`, xdes:`${orderD}`, 
+                                            xavail:`${orderAvail}`, xprice:`${orderPrice}`, 
+                                            xdate:`${orderDate}`, xID:`${orderID}`, xaddy:`${orderAddy}`}}
+                            />
                         ))}
                     </div>
                     ):(
